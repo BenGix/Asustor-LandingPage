1f1c6f8724d07acaf29e8f28a770ec649df956f6
Farzad Jambor <farzadjambor98@gmail.com>
2023-12-21 12:05:14 +0330

    final

---
+++ assets/js/gsap.js
@@ -0,0 +1,26 @@
+gsap.to(".first-row", {
+    scrollTrigger: {
+        trigger: ".first-row",
+        start: "top bottom",
+        scrub: true,
+    },
+    x: 500,
+});
+
+gsap.to(".second-row", {
+    scrollTrigger: {
+        trigger: ".second-row",
+        start: "top bottom",
+        scrub: false,
+    },
+    x: -500,
+});
+
+gsap.to(".third-row", {
+    scrollTrigger: {
+        trigger: ".second-row",
+        start: "top bottom",
+        scrub: true,
+    },
+    x: 500,
+});
